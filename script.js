7166750c22585142a61dd86bc12acd9a99781691
Gopal Patel <130164161+Gopalptl1405@users.noreply.github.com>
2023-05-21 20:19:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function getFormvalue() {
     //Write your code here
     var fname = document.getElementById("fname").value;
 	var lname = document.getElementByid("lname").value;
-	aler(fname +" " + lname);
+	alert("hello");
 }
