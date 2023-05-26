15dc6b8e427bead580b9c902e2a3126be95cf246
Gopal Patel <130164161+Gopalptl1405@users.noreply.github.com>
2023-05-26 20:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function getFormvalue() {
     //Write your code here
- //    const fname = document.getElementById("fname").value;
-	// const lname = document.getElementByid("lname").value;
-	// console.log(fname+" "+lname);
-	// alert(fname+" "+lname);
+    const fname=document.getElementById("fname").value;
+	const lname=document.getElementById("lname").value;
+	console.log(fname+" "+lname);
+	alert(fname+" "+lname);
 	
-const fname=document.getElementById("fname").value;
- const lname=document.getElementById("lname").value;
+// const fname=document.getElementById("fname").value;
+//  const lname=document.getElementById("lname").value;
     
-console.log(fname+" "+lname);
-alert(fname+" "+lname);
+// console.log(fname+" "+lname);
+// alert(fname+" "+lname);
 
 
 };
